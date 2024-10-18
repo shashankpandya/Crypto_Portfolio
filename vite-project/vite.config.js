41ac2c7d61f847968f8a9226a4f0506df9e64257
Shashank pandya <pandyashashank1@gmail.com>
2024-10-18 12:49:33 +0530

    minorchangeindeploy

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { createProxyMiddleware } from "http-proxy-middleware";
+// import { createProxyMiddleware } from "http-proxy-middleware";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,9 +17,7 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    rollupOptions: {
-      external: ["react-tooltip"],
-    },
-  },
+  optimizeDeps: {
+    include: ['react-tooltip']
+  }
 });
