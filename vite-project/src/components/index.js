d1b9d0db0439cfd4475ef835dac20dee37f872dd
shashankpandya <pandyashashank1@kgpian.iitkgp.ac.in>
2026-08-22 12:26:38 +0530

    chore(client): delete dead entrypoint, service worker, and barrel file

--- vite-project/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Home } from "./Homepage/Home";
-export { default as Watchlist } from "./Watchlist";
-export { default as TokenTransfer } from "./TokenTransfer";
-export { default as AllowanceManager } from "./AllowanceManager";
-export { default as CoinDetails } from "./CoinDetails";
-export { default as TopCoins } from "./TopCoins";
-export { default as ErrorBoundary } from "./ErrorBoundary";
-export { default as AdminPanel } from "./AdminPanel";
